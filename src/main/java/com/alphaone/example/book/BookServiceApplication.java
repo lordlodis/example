6f4a9bdc0435b568f2980af92420a5c98376fc0e
lordlodis <ngocna@gmail.com>
2021-04-18 13:42:44 +0200

    Added logging

--- src/main/java/com/alphaone/example/book/BookServiceApplication.java
+++ src/main/java/com/alphaone/example/book/BookServiceApplication.java
@@ -1,5 +1,7 @@
 package com.alphaone.example.book;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
@@ -8,7 +10,10 @@ import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 @EnableEurekaClient
 public class BookServiceApplication {
 
+	private static Logger LOGGER = LoggerFactory.getLogger(BookServiceApplication.class);
+	
 	public static void main(String[] args) {
+		LOGGER.info("Starting up service " + BookServiceApplication.class.getName());		
 		SpringApplication.run(BookServiceApplication.class, args);
 	}
 }
